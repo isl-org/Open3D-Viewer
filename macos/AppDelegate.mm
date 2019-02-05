b2e71b972a39ee374667ffd55b4519781a8c467e
Yixing Lao <yixing.lao@gmail.com>
2019-02-05 03:50:10 -0800

    clean comments

--- macos/AppDelegate.mm
+++ macos/AppDelegate.mm
@@ -26,7 +26,6 @@
 
 #import "AppDelegate.h"
 #import "string"
-// #import "auto_visualize.h"
 
 @implementation AppDelegate
 
@@ -34,21 +33,6 @@
 - (void)applicationDidFinishLaunching:(NSNotification*)aNotification {
     // Insert code here to initialize your application
 }
-
-// - (void)run_thread:(NSString*)filename {
-//     NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-
-//     // Redirect log for debugging
-//     int fd = creat("/Users/ylao/repo/Open3D-Viewer/log.txt",
-//                    S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-//     close(STDERR_FILENO);
-//     dup(fd);
-//     close(fd);
-//     NSLog(@"%@", filename);
-//     auto_visualize(std::string([filename UTF8String]));
-
-//     // [pool drain];
-// }
 
 - (BOOL)application:(NSApplication*)sender openFile:(NSString*)filename {
     // Redirect log for debugging
@@ -58,31 +42,12 @@
     // dup(fd);
     // close(fd);
 
-    // Creating new thrads in Obj-C
-    // https://stackoverflow.com/questions/5558950/how-to-pass-an-argument-through-nsthread
-    // [NSThread detachNewThreadSelector:@selector(run_thread:)
-    //                          toTarget:self
-    //                        withObject:filename];
-    // auto_visualize(std::string([filename UTF8String]));
-
     // Get full command
     NSString* resource_path = [[NSBundle mainBundle] resourcePath];
     NSString* open3d_viewer_path = [NSString
             stringWithFormat:@"%@/%@", resource_path, @"open3d-viewer-core"];
     NSString* full_command =
             [NSString stringWithFormat:@"%@ %@", open3d_viewer_path, filename];
-
-    // [NSString stringWithFormat:@"%@ %@", open3d_viewer_path, filename];
-    // https://stackoverflow.com/questions/8001677/how-do-i-convert-a-nsstring-into-a-stdstring
-    // https://stackoverflow.com/questions/3552195/how-to-convert-stdstring-to-nsstring
-    // std::string full_command_std =
-    // concat_string(std::string([open3d_viewer_path UTF8String]),
-    //                                              std::string(" "));
-    // full_command_std = concat_string(full_command_std, std::string([filename
-    // UTF8String])); NSString *full_command = [NSString
-    // stringWithCString:full_command_std.c_str()
-    //                           encoding:[NSString defaultCStringEncoding]];
-    // NSLog(@"%@", full_command);
 
     NSTask* task = [[NSTask alloc] init];
     [task setLaunchPath:@"/bin/bash"];
